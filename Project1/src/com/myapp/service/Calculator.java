88017ebf68097e68da908e516ff8fd9f08a39d16
AbhishekMishra <abhishekmishra968517@gmail.com>
2025-01-08 20:24:23 +0530

    Dev1 Create Calculator class and modified WelcomeMessage class

---
+++ Project1/src/com/myapp/service/Calculator.java
@@ -0,0 +1,7 @@
+package com.myapp.service;
+
+public class Calculator {
+	public long sum(long x, long y) {
+		return x+y;
+	}
+}
--- Project1/src/com/myapp/service/WelcomeMessage.java
+++ Project1/src/com/myapp/service/WelcomeMessage.java
@@ -5,6 +5,8 @@ public class WelcomeMessage {
 	public static void main(String[] args) {
 		System.out.println("Hello Teams Welcome For This project");
 		System.out.println("Good morning");
+		Calculator cal = new Calculator();
+		System.out.println(cal.sum(10, 29));
 
 	}
 
