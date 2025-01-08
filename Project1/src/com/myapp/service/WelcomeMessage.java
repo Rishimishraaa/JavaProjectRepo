76ccb02aa799e9e47d978307567e34dffbcc7297
AbhishekMishra <abhishekmishra968517@gmail.com>
2025-01-08 20:13:47 +0530

    Hello

---
+++ Project1/src/com/myapp/service/WelcomeMessage.java
@@ -0,0 +1,11 @@
+package com.myapp.service;
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Teams Welcome For This project");
+		System.out.println("Good morning");
+
+	}
+
+}
